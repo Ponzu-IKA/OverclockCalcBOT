f449e4f22b9ff38303023ff11c17dd62e2e8027f
Ponzu-IKA <jsoajod@outlook.jp>
2024-07-04 17:08:51 +0900

    変更ミス

--- src/main/kotlin/ponzu_ika/occalc/Main.kt
+++ src/main/kotlin/ponzu_ika/occalc/Main.kt
@@ -1,4 +1,4 @@
-package ponzu_ika.sensitive_killer
+package ponzu_ika.occalc
 
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
--- src/main/kotlin/ponzu_ika/occalc/SlashCommands.kt
+++ src/main/kotlin/ponzu_ika/occalc/SlashCommands.kt
@@ -1,4 +1,4 @@
-package ponzu_ika.sensitive_killer
+package ponzu_ika.occalc
 
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.MessageEmbed
